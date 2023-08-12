758637e15be354857766a421430f0f744c436d26
claudiajstevens <claudiajstevens@gmail.com>
2023-08-11 17:02:22 -0700

    fix: fixing bugs for user security

--- saywhonow_backend/src/main/java/com/example/saywhonow_backend/User.java
+++ saywhonow_backend/src/main/java/com/example/saywhonow_backend/User.java
@@ -67,37 +67,41 @@ public class User implements UserDetails {
         this.genres = genres;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
+    // @Override
+    // public Collection<? extends GrantedAuthority> getAuthorities() {
         
-        List<GrantedAuthority> roles = new ArrayList<>();
-        roles.add(new Authority("ROLE_USER"));
-        return roles;
-    }
+    //     List<GrantedAuthority> roles = new ArrayList<>();
+    //     roles.add(new Authority("ROLE_USER"));
+    //     return roles;
+    // }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+    // @Override
+    // public boolean isAccountNonExpired() {
+    //     return true;
+    // }
         
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+    // @Override
+    // public boolean isAccountNonLocked() {
+    //     return true;
+    // }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+    // @Override
+    // public boolean isCredentialsNonExpired() {
+    //     return true;
+    // }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+    // @Override
+    // public boolean isEnabled() {
+    //     return true;
+    // }
 
     public void setAuthorities(List<Authority> authorities) {
         this.authorities = authorities;
+    }
+
+    public static Object withDefaultPasswordEncoder() {
+        return null;
     }
 
 
--- saywhonow_backend/src/main/java/com/example/saywhonow_backend/config/SecurityConfig.java
+++ saywhonow_backend/src/main/java/com/example/saywhonow_backend/config/SecurityConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import com.example.saywhonow_backend.User;
 
 @Configuration
 public class SecurityConfig {
@@ -18,22 +22,22 @@ public class SecurityConfig {
     // @Autowired
     // private UserDetailsService userDetailsService;
     
-    @Autowired
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // auth
-        // .userDetailsService(userDetailsService);
+    // @Autowired
+    // public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //     // auth
+    //     // .userDetailsService(userDetailsService);
       
-        auth
-        .ldapAuthentication()
-          .userDnPatterns("uid={0},ou=people")
-          .groupSearchBase("ou=groups")
-          .contextSource()
-            .url("ldap://localhost:8389/dc=springframework,dc=org")
-            .and()
-          .passwordCompare()
-            .passwordEncoder(new BCryptPasswordEncoder())
-            .passwordAttribute("userPassword");
-    }
+    //     auth
+    //     .ldapAuthentication()
+    //       .userDnPatterns("uid={0},ou=people")
+    //       .groupSearchBase("ou=groups")
+    //       .contextSource()
+    //         .url("ldap://localhost:8389/dc=springframework,dc=org")
+    //         .and()
+    //       .passwordCompare()
+    //         .passwordEncoder(new BCryptPasswordEncoder())
+    //         .passwordAttribute("userPassword");
+    // }
 
     
     @Bean
@@ -45,6 +49,19 @@ public class SecurityConfig {
             .httpBasic(withDefaults());
         return http.build();
     }
+
+
+    	@Bean
+	    public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.setUsername("user")
+				.setPassword("password")
+				.setAuthorites("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
 
     
 }
